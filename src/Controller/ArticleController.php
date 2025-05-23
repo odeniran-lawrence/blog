25e76e3bc3b3d4a3d9e0a4a900b614cbf23e1c2b
lawrence <odelaw9@gmail.com>
2025-05-23 15:36:21 +0200

    création de page et route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,32 +3,36 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/article')]
 final class ArticleController extends AbstractController
 {
+    /**
+     * Le constructeur permet de déclarer les dépendances une fois
+     * et d'éviter la non-application du concept DRY (Don't Repeat Yourself)
+     */
+    public function __construct(
+        private ArticleRepository $ar, // Repository de l'entité Article
+        private EntityManagerInterface $em // Gestionnaire d'entités avec Doctrine
+    ) {}
+
     // Route "/articles" menant à la liste des articles
     #[Route('s', name: 'articles', methods: ['GET'])]
     public function index(
-        ArticleRepository $ar,// Repossitory de l'entité Article
-        PaginatorInterface $paginator,// Classe pour la fonctionnalité de pagination
-        Request $request // Classe pour récupérer les paramètre de la requete HTTP
+        PaginatorInterface $paginator, // Classe pour la fonctionnalité de pagination
+        Request $request // Classe pour récupérer les paramètres de la requête HTTP
     ): Response {
-        $all = $ar->findBy(
-            ['is_published' => true,
-            'is_archived' => false,
-        ],
-        ['title' => 'asc']);
-
+        $all = $this->ar->findBy(['is_published' => true, 'is_archived' => false]);
         $pagination = $paginator->paginate(
             $all,
-            $request->query->getInt('page', 1),/* page number */
-            12 /* limit per page */
+            $request->query->getInt('page', 1),
+            100
         );
 
         return $this->render('article/index.html.twig', [
@@ -37,33 +41,25 @@ final class ArticleController extends AbstractController
         ]);
     }
 
-    //Route "/article/slug" menant a la page d'un article
-    #[Route('/{slug}', name:'article', methods:['GET'])]
-    public function view(): Response
+    #[Route('/{slug}', name: 'article', methods: ['GET'])]
+    public function view(string $slug): Response
     {
-          return $this->render('article/index.html.twig', [
-            // 'articles' => $article
+        return $this->render('article/view.html.twig', [
+            'article' => $this->ar->findOneBySlug($slug)
         ]);
     }
 
-    //Route "/article/slug/edit" menant a la page d'un article
-    #[Route('/{slug}/edit', name:'article_edit', methods:['GET','POST'])]
+    #[Route('/{slug}/edit', name: 'article_edit', methods: ['GET', 'POST'])]
     public function edit(): Response
     {
-          return $this->render('article/edit.html.twig', [
-            // 'articles' => $article
+        return $this->render('article/edit.html.twig', [
+            // 'article' => $article
         ]);
     }
 
-    // Route "/article/slug" menant a la page d'un article
-    #[Route('/{slug}/delete', name:'article', methods:['POST'])]
+    #[Route('/{slug}/delete', name: 'article_delete', methods: ['POST'])]
     public function delete(): Response
     {
-          return $this->redirectToRoute('articles');
+        return $this->redirectToRoute('articles');
     }
-
-
-
-
 }
-
