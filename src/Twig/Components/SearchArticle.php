e2e1d46b1e5024d8a7af2dbb7495d7c7f58f992c
lawrence <odelaw9@gmail.com>
2025-05-30 11:51:28 +0200

    resarch et fin de pagination

---
+++ src/Twig/Components/SearchArticle.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+namespace App\Twig\Components;
+
+use App\Service\UxPackageRepository;
+use App\Repository\ArticleRepository;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+
+#[AsLiveComponent('SearchArticle', template: 'components/SearchArticle.html.twig')]
+final class SearchArticle
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true, url: true)]
+    public ?string $query = null;
+
+    public function __construct(private ArticleRepository $ar)
+    {
+    }
+
+    public function getArticles(): array
+    {
+        if ($this->query){// s'il y une requête, on cherche les articles correspondans
+        return $this->ar->findAll($this->query);
+    }
+
+    return $this->ar->findby([],['created_at' => 'DESC'], 10);
+
+
+    }
+}
+
+/**
+     * LiveProp est une classe qu'on utilise en annotation pour définir les propriétés "Live"
+     * que l'on va utiliser dans le composant. C'est comme le passage de props en JavaScript.
+     * 
+     * "writable : true" signifie que la propriété est modifiable depuis le composant.
+     * "url : true" signifie que la propriété sera disponible dans l'URL.
+     */
+    
+
